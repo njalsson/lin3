591a83ac87d1c3ae541ba02bcf488a19fb6682ea
njalsson <fridriknj339@nemi.tskoli.is>
2017-12-07 21:22:07 +0000

    did program 1 in the exam 1

---
+++ verk/prof1.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "there is a command that shows list of every user logged in, users"
+echo "than you can pipe that into wc, which is a word counting program."
+
+echo "so users | wc -w outputs"
+echo users | wc -w
